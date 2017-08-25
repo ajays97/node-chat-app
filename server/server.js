fd32a10e538c5d35e7cdc060a33d9b240eef090a
Ajay Srinivas <ajay.s1997@gmail.com>
2017-08-25 15:57:55 +0530

    Add geolocation support via geolocation api

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 
-const {generateMessage} = require('./utils/message');
+const {generateMessage, generateLocationMessage} = require('./utils/message');
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 
@@ -16,21 +16,18 @@ app.use(express.static(publicPath));
 io.on('connection', function (socket) {
   console.log('A new user connected.');
 
-  socket.emit('newMessage', generateMessage('Ajay Srinivas', 'Welcome to Radian Chat!'));
+  socket.emit('newMessage', generateMessage('AJ', 'Welcome to Radian Chat!'));
 
-  socket.broadcast.emit('newMessage', generateMessage('Ajay Srinivas', 'A fucker joined.'));
+  socket.broadcast.emit('newMessage', generateMessage('AJ', 'A fucker joined.'));
 
   socket.on('createMessage', function (message, callback) {
     console.log('createMessage', message);
     io.emit('newMessage', generateMessage(message.from, message.text));
     callback('This is from the server.');
+  });
 
-    // socket.broadcast.emit('newMessage', {
-    //   from: message.from,
-    //   text: message.text,
-    //   createdAt: new Date().getTime()
-    // });
-
+  socket.on('createLocationMessage', function (coords) {
+    io.emit('newLocationMessage', generateLocationMessage('AJ', coords.latitude, coords.longitude));
   });
 
   socket.on('disconnect', function () {

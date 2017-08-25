9621fc90d08c841aed7433f4e333c43c6ecf1e76
Ajay Srinivas <ajay.s1997@gmail.com>
2017-08-25 20:00:00 +0530

    Add Users class and updateUserList event

--- server/server.js
+++ server/server.js
@@ -4,21 +4,36 @@ const express = require('express');
 const socketIO = require('socket.io');
 
 const {generateMessage, generateLocationMessage} = require('./utils/message');
+const {isRealString} = require('./utils/validation');
+const {Users} = require('./utils/users');
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 
 var app = express();
 var server = http.createServer(app);
 var io = socketIO(server);
+var users = new Users();
 
 app.use(express.static(publicPath));
 
 io.on('connection', function (socket) {
   console.log('A new user connected.');
 
-  socket.emit('newMessage', generateMessage('AJ', 'Welcome to Radian Chat!'));
+  socket.on('join', function (params, callback) {
+    if (!isRealString(params.name) || !isRealString(params.room)) {
+      return callback('Name and room name are required.');
+    }
 
-  socket.broadcast.emit('newMessage', generateMessage('AJ', 'A fucker joined.'));
+    socket.join(params.room);
+    users.removeUser(socket.id);
+    users.addUser(socket.id, params.name, params.room);
+
+    io.to(params.room).emit('updateUserList', users.getUserList(params.room));
+    socket.emit('newMessage', generateMessage('GeekBot', 'Welcome to Radian Chat!'));
+    socket.broadcast.to(params.room).emit('newMessage', generateMessage('GeekBot', `${params.name} has joined.`));
+
+    callback();
+  });
 
   socket.on('createMessage', function (message, callback) {
     console.log('createMessage', message);
@@ -27,11 +42,17 @@ io.on('connection', function (socket) {
   });
 
   socket.on('createLocationMessage', function (coords) {
-    io.emit('newLocationMessage', generateLocationMessage('AJ', coords.latitude, coords.longitude));
+    io.emit('newLocationMessage', generateLocationMessage('GeekBot', coords.latitude, coords.longitude));
   });
 
   socket.on('disconnect', function () {
     console.log('Client disconnected');
+    var user = users.removeUser(socket.id);
+
+    if (user) {
+      io.to(user.room).emit('updateUserList', users.getUserList(user.room));
+      io.to(user.room).emit('newMessage', generateMessage('GeekBot', `${user.name} has left.`));
+    }
   });
 });
 
